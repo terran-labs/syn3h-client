c1bab1de45247ce4bcbddd9b4fef29cfb3bfc36f
Aubrey Falconer <a@aubreyfalconer.com>
2022-06-01 13:32:30 -0700

    Ongoing submodule organizational updates

--- _Shell/SymlinkProprietaryAssets.sh
+++ _Shell/SymlinkProprietaryAssets.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 #
-# Symlink contents of _Proprietary_* into Assets directory,
+# Symlink contents of Proporietary _Prop* submodules into Assets directory,
 # then .gitignore them to prevent proprietery assets from being
-# accidentally included in our public syn3h-player repo.
+# accidentally included in our public syn3h-client repo.
 
 # Symlink everything from the _Proprietary_* folder into our Assets folder
 echo "Script executed from: ${PWD}"
 
-ln -v -s _Proprietary_Resources/Assets/* Assets/
-ln -v -s _Proprietary_Libraries/Assets/* Assets/
+ln -v -s _PropResources/Assets/* Assets/
+ln -v -s _PropLibs/Assets/* Assets/
 
 # Add everything from the _Proprietary_* folders to our root .gitignore as well
 # Props:
@@ -17,7 +17,6 @@ ln -v -s _Proprietary_Libraries/Assets/* Assets/
 # - https://stackoverflow.com/questions/3557037/appending-a-line-to-a-file-only-if-it-does-not-already-exist/28021305
 SAVEIFS=$IFS
 IFS=$'\n'
-# for f in $(find ../_Proprietary_*/* -maxdepth 1 -type l); do
 for f in $(find [Aa]ssets/* -maxdepth 1 -type l); do
 	gitignore_line="/[Aa]ssets/${f:7}"
 	grep -qxF "$gitignore_line" ./.gitignore || (echo "$gitignore_line" >>./.gitignore && echo "> $gitignore_line added to .gitignore")
